4d20b2206ee804933230524a15db837a1c44fb41
Jason Rodrigues <69276260+JasonRodrigues15@users.noreply.github.com>
2020-09-13 00:19:46 -0400

    Status and test statement
    This function makes the bots presence online and prints "The bot is ready to run" for testing purposes to see if its working properly.

--- main.py
+++ main.py
@@ -7,3 +7,8 @@ from itertools import cycle
 client = commands.Bot(command_prefix=".")
 token = open("token.txt", "r").read()
 client.run(token)
+@client.event
+async def on_ready():
+    await client.change_presence(status=discord.Status.online, activity=discord.Game(""))
+    change_status.start()
+    print('The bot is ready to run.')
