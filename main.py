0d5583501580b155b91d523e46586696c51663cb
Jason Rodrigues <69276260+JasonRodrigues15@users.noreply.github.com>
2020-09-13 00:33:46 -0400

    User leave and join
    Sends a message when a member of the server leaves or joins the server

--- main.py
+++ main.py
@@ -16,3 +16,9 @@ async def on_ready():
 @tasks.loop(seconds=3)
 async def change_status():
     await client.change_presence(activity=discord.Game(next(status)))
+@client.event
+async def on_member_join(member):
+    print(f"{member} has joined a server")
+@client.event
+async def on_member_remove(member):
+    print(f"{member} has left the server")
