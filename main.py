f41b8ee0876ea74b51ddd3077aef582b53ad469e
Jason Rodrigues <69276260+JasonRodrigues15@users.noreply.github.com>
2020-09-13 00:22:57 -0400

    Cycle Status
    This function changes the bots status every 3 seconds. It cycles between '.info for information' and '.help for commands'.

--- main.py
+++ main.py
@@ -7,8 +7,12 @@ from itertools import cycle
 client = commands.Bot(command_prefix=".")
 token = open("token.txt", "r").read()
 client.run(token)
+status = cycle(['.info for information', '.help for commands'])
 @client.event
 async def on_ready():
     await client.change_presence(status=discord.Status.online, activity=discord.Game(""))
     change_status.start()
     print('The bot is ready to run.')
+@tasks.loop(seconds=3)
+async def change_status():
+    await client.change_presence(activity=discord.Game(next(status)))
